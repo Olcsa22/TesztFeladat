a76b67213f2b2562d495a711089cfb82865bbae0
Olcsa22 <74309417+Olcsa22@users.noreply.github.com>
2022-08-11 23:23:34 +0200

    Initial commit

---
+++ src/main/java/hu/oliver/tesztfeladatweblap/TesztFeladatWeblapApplication.java
@@ -0,0 +1,13 @@
+package hu.oliver.tesztfeladatweblap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesztFeladatWeblapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesztFeladatWeblapApplication.class, args);
+	}
+
+}
